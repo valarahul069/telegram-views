b799c6f35a48c59b984b41340889d4ddf2eece93
valarahul069 <123723850+valarahul069@users.noreply.github.com>
2023-03-06 19:48:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from configparser import ConfigParser
 from os import system, name
 from threading import Thread, active_count
 from re import search, compile
+import sys
+
+post_url = sys.argv[1]
+
 
 THREADS = 5000
 PROXIES_TYPES = ('http', 'socks4', 'socks5')
